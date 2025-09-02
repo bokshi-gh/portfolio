9ef322ef4657a443fed16497ac0b3a0da20e2919
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-09-02 07:23:54 +0545

    add blog in index

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+let blogsContainer = document.getElementById("blogs");
+
+async function loadBlog(){
+	try {
+	    const response = await fetch('../jsons/blogs.json');
+	    const blogs = await response.json();
+            console.log(blogs)
+
+	    if (blogs) {
+	      
+	      blogs.array.forEach(blog => {
+				blogsContainer += `
+                
+		<p><a href="/${blog.title}">${blog.title}</a> | ${blog.date}</p>
+	      `;
+	      });
+	    } else {
+	      blogsContainer.innerHTML = `<p>Blog not found.</p>`;
+	    }
+	  } catch (err) {
+	    console.error(err);
+	  }
+}
+
+loadBlog();
