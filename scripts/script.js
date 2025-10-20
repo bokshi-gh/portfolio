fa7a38d9af297fc81e06791e56ae9ae0d0237cdc
Rajesh Thapa <www.iamrajeshthapa69@gmail.com>
2025-10-20 18:23:53 +0545

    change structure

--- scripts/script.js
+++ scripts/script.js
@@ -4,12 +4,12 @@ const dob = new Date("2006-02-28T00:00:00Z");
 const secondsInYear = 365.2425 * 24 * 60 * 60;
 
 function updateAge() {
-    const now = new Date();
-    const ageInSeconds = (now - dob) / 1000; // milliseconds → seconds
-    const ageInYears = ageInSeconds / secondsInYear;
-  
-    // Display with 12 decimal places
-    ageDiv.textContent = ageInYears.toFixed(12);
+  const now = new Date();
+  const ageInSeconds = (now - dob) / 1000; // milliseconds → seconds
+  const ageInYears = ageInSeconds / secondsInYear;
+
+  // Display with 12 decimal places
+  ageDiv.textContent = ageInYears.toFixed(12);
 }
 
 // Call every 1 milliseconds for smooth update
