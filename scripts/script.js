01ddbfde2189b4e0a3436b3c97667f369b410ea3
bokshi <devrajeshthapa@gmail.com>
2025-10-20 19:07:44 +0000

    ui improvements

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,23 @@ const pulse = document.getElementsByClassName("pulse")[0];
 const ageDiv = document.getElementsByClassName("age")[0]; // get first .age element
 
 const isAlive = true;
-
 const dob = new Date("2006-02-28T00:00:00Z");
 const secondsInYear = 365.2425 * 24 * 60 * 60;
 
+if (isAlive) {
+  pulse.classList.remove("dead");
+  pulse.classList.add("alive");
+} else {
+  pulse.classList.remove("alive");
+  pulse.classList.add("dead");
+}
+
+let firstTime = true;
 function updateAge() {
+  if (!firstTime) {
+    if (!isAlive) return; // if not alive(dead) dont update the age
+  } else firstTime = false;
+
   const now = new Date();
   const ageInSeconds = (now - dob) / 1000; // milliseconds → seconds
   const ageInYears = ageInSeconds / secondsInYear;
