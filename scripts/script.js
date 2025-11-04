c235db7f4336cc3ab39f7e57ed651b3d36ca0c5c
bokshi-gh <devrajeshthapa@gmail.com>
2025-11-04 15:53:33 +0545

    banner

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+if (window.innerWidth <= 400) document.getElementById("banner").setAttribute("src", "assets/banner-mobile.jpg");
+
 const ageDiv = document.getElementsByClassName("age")[0];
 
 const dob = new Date("2006-02-28T00:00:00Z");
