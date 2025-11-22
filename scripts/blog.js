984d365597d41f22503536008dbe073b06c9856f
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-22 22:35:40 +0545

    update

--- scripts/blog.js
+++ scripts/blog.js
@@ -6,29 +6,35 @@ blogDate.innerText = "fetching blog contents..."
 
 const fetchBlog = async (linkTitle) => {
     const url = `https://raw.githubusercontent.com/bokshi-gh/portfolio/main/blogs/${linkTitle}`;
+
     try {
         const response = await fetch(url);
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const raw = await response.text();
-        const indexOfFirstCariageReturn = raw.indexOf("\r\n");
-        const indexOfFirstDoubleCariageReturn = raw.indexOf("\r\n\r\n");
+        raw = raw.replace(/\r\n/g, "\n");
+
+        const indexOfFirstNewline = raw.indexOf("\n");
+        const indexOfSecondNewline = raw.indexOf("\n\n");
 
         const title = raw.slice(7, indexOfFirstCariageReturn);
         document.title = title + " | " + "Rajesh Thapa";
-        const date = raw.slice(indexOfFirstCariageReturn + 7, indexOfFirstDoubleCariageReturn);
-        const content = raw.slice(indexOfFirstDoubleCariageReturn + 4, raw.length);
+
+        const date = raw.slice(indexOfFirstNewline + 7, indexOfSecondNewline);
+        const content = raw.slice(indexOfSecondNewline + 4, raw.length);
 
         blogDate.innerText = "";
 
         blogTitle.innerText = title;
-        blogDate.innerHTML = `<i class="fa-solid fa-calendar-days"></i>` + date;
+        blogDate.innerHTML = `<i class="fa-solid fa-calendar"></i>` + date;
         blogContent.innerHTML = content;
     } catch (error) {
         blogDate.innerText = "";
         blogDate.innerText = `Failed to fetch Blog contents\r\nEither the blog you are trying to find was deleted/migrated or simply didn't existed\r\n${error}`;
+        
         console.error('Failed to fetch Blog:', error);
     }
 
--- scripts/blogs.js
+++ scripts/blogs.js
@@ -2,32 +2,36 @@ const blogList = document.getElementsByClassName("blog-list")[0];
 
 const fetchBlog = async (data) => {
     blogList.innerText = "";
+
     for (blog of data) {
         try {
             const response = await fetch(blog.download_url);
+
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
-            const raw = await response.text();
-            const indexOfFirstCariageReturn = raw.indexOf("\r\n");
-            const indexOfFirstDoubleCariageReturn = raw.indexOf("\r\n\r\n");
-            console.log(indexOfFirstCariageReturn, indexOfFirstDoubleCariageReturn);
+            let raw = await response.text();
+            raw = raw.replace(/\r\n/g, "\n");
 
-            const title = raw.slice(7, indexOfFirstCariageReturn);
-            const date = raw.slice(indexOfFirstCariageReturn + 7, indexOfFirstDoubleCariageReturn);
+            const indexOfFirstNewline = raw.indexOf("\n");
+            const indexOfSecondNewline = raw.indexOf("\n\n");
+
+            const title = raw.slice(7, indexOfFirstNewline);
+            const date = raw.slice(indexOfFirstNewline + 7, indexOfSecondNewline);
 
             const linkTitle = blog.name;
             const element = `
                 <div class="blog">
                     <a href="blog.html?title=${linkTitle}">${title}</a>
-                    <span><i class="fa-solid fa-calendar-days"></i> ${date}</span>
+                    <span><i class="fa-solid fa-calendar"></i> ${date}</span>
                 </div>
             `
 
             blogList.innerHTML += element;
         } catch (error) {
             blogList.innerText = `Failed to fetch Blogs\r\n${error}`;
+            
             console.error('Failed to fetch Blog:', error);
         }
     }
@@ -38,7 +42,9 @@ const fetchBlogs = async () => {
 
     try {
         blogList.innerText = "fetching blogs...";
+
         const response = await fetch(url);
+
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -48,6 +54,7 @@ const fetchBlogs = async () => {
     } catch (error) {
         blogList.innerText = "";
         blogList.innerText = `Failed to fetch Blogs\r\n${error}`;
+
         console.error('Failed to fetch Blogs:', error);
     }
 }
